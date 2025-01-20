cd1ec85caecfb84a54b567810be7903373d7b98d
aniolnicolau <aniolnicolauroig@gmail.com>
2025-01-20 09:43:03 +0100

    PR3 Arrays-PHP - finished

--- Arrays.php
+++ Arrays.php
@@ -9,7 +9,7 @@
 
 
 <body>
-    <h1>EJERCICO 1</h1>
+    <h1>EJERCICIO 1</h1>
     <?php
     $array = array("dato 1º" => "Sara", "dato 2º" => "Martinez", "dato 3º" => 23, "dato 4º" => "Barcelona");
     //    var_dump(value: $array);
@@ -21,7 +21,7 @@
     <br>
 
 
-    <h1>EJERCICO 2</h1>
+    <h1>EJERCICIO 2</h1>
     <?php
 
     $array2 = array("Nombre" => "Sara", " Apellido" => "Martinez", " Edad" => 23, " Ciudad" => "Barcelona");
@@ -33,7 +33,7 @@
     <br>
 
 
-    <h1>EJERCICO 3</h1>
+    <h1>EJERCICIO 3</h1>
     <?php
     $array["dato 3º"] = 24;
     foreach ($array as $key => $value) {
@@ -41,14 +41,14 @@
     }
     ?>
 
-    <h1>EJERCICO 4</h1>
+    <h1>EJERCICIO 4</h1>
     <?php
     $array4 = array("Nombre" => "Sara", " Apellido" => "Martinez", " Edad" => 24, " Ciudad" => "Barcelona");
     unset($array4[" Ciudad"]);
     var_dump(value: $array4);
     ?>
 
-    <h1>EJERCICO 5</h1>
+    <h1>EJERCICIO 5</h1>
     <?php
 
     $result;
@@ -74,7 +74,7 @@
     echo "<br>";*/
     ?>
 
-    <h1>EJERCICO 6</h1>
+    <h1>EJERCICIO 6</h1>
     <?php
     echo "Notas de los estudiantes: ";
     $array6 = array("Miguel: " => 5, "Luís: " => 7, "Marta: " => 10, "Isabel: " => 8, "Aitor" => 4, "Pepe" => 1);
@@ -85,18 +85,26 @@
     }
 
     ?>
-    <h1>EJERCICO 7</h1>
+    <h1>EJERCICIO 7</h1>
     <?php
     $suma = array_sum($array6);
     $media = $suma / count($array6);
-
-
-    echo "Media de las notas: " . $media . "<br>";
-    if ($array6 > $media) {
-        echo "Alumnos con nota por encima de la media: <br>";
-
+    echo "Media de las notas: " . number_format($media, 2) . "<br>";
+    echo "Alumnos con nota por encima de la media: <br>";
+    foreach ($array6 as $nombre => $nota) {
+        if ($nota > $media) {
+            echo $nombre . ": " . $nota . "<br>";
+        }
     }
     ?>
+    <h1>EJERCICIO 8</h1>
+    <?php
+    $nota_mas_alta = max($array6);
+    $mejor_alumno = array_search($nota_mas_alta, $array6);
+    echo "La nota más alta es: " . $nota_mas_alta . "<br>";
+    echo "El mejor alumno de la clase es: " . $mejor_alumno
+    ?>
+
 </body>
 
 </html>
